6845e69b392424061fd2be72a785a1ec4836a3d9
Neal <tnharvey@gmail.com>
2018-10-29 13:54:34 +0800

    "It works! No layout or buttons, but grid works in black and white with mouseEnter listener."

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,51 @@
 // Etch-a-Sketch project for the the Odin Project.
 
 let documentHeight = document.documentElement.clientHeight;
-let documentWidth = document.documentElement.clientWidth;
-let grid = 16;
-let gridSize = documentHeight - (documentHeight * 0.2); // Assuming the rest of the page is only 20%
-let gridPointSize = gridSize / grid;
+let gridContainerHeight = documentHeight - (documentHeight * 0.2); // Assuming the rest of the page is only 20%
+let grid = 32;
+let padding = 1; // // TODO: Capture this from the CSS
+let totalPadding = grid * padding + padding;
+let cellHeight = (gridContainerHeight-totalPadding)/grid;
+let colorScheme = "BW";
+
+setGridProperties();
+generateGridElements();
+
+function setGridProperties () {
+  // set gridContainer properties
+  document.getElementById("gridContainer").style.cssText = (`display: grid;
+    grid-template-columns: repeat(` + grid + `,`+ cellHeight + `px);
+    grid-template-rows: repeat(` + grid + `,` + cellHeight + `px);
+    grid-gap: ` + padding + `px;`);
+
+}
+
+function generateGridElements () {
+  // function to generate grid elements based on given parameters.
+
+  for (var i = 0; i < (grid * grid); i++) {
+    let div = document.createElement('div');
+
+    div.id = toString(i);
+    document.getElementById("gridContainer").appendChild(div);
+
+    div.addEventListener("mouseenter", changeCellColor);
+  }
+}
+
+// function to generate listeners for each div inside gridContainer
+
+// function to capture and assign mouseover grid coordinates (determine grid cell)
+
+function changeCellColor (e) {
+  if (colorScheme === "BW") {
+    let thisDiv = e.target;
+
+    thisDiv.style.backgroundColor = "black";
+  }
+}
+// function to determine color of grid cell
+
+// function to update grid when ChangeGrid is clicked
+
+// function to clear/reset the existing grid. A good would be to clear color classes or to reset all rgb for cell to default
